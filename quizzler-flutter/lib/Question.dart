6106f9a8f95a4765c4f043ceafbd88d8685a3d48
ashishsahu1 <ashishsahu1999@hotmail.com>
2020-08-14 12:50:57 +0530

    quizzler

---
+++ quizzler-flutter/lib/Question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String quesText;
+  bool quesAns;
+
+  Question({String q, bool a}) {
+    quesText = q;
+    quesAns = a;
+  }
+}
---
+++ quizzler-flutter/lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'Question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+/*List<Widget> scoreKeeper = [];
+List ques = [
+  'You can lead a cow down stairs but not up stairs.',
+  'Approximately one quarter of human bones are in the feet.',
+  'A slug\'s blood is green.'
+];
+
+List ans = ['False', 'True', 'False'];
+
+int qno = 0;
+
+Question q1 = new Question(
+    q: 'You can lead a cow down stairs but not up stairs.', a: false);
+Question q1 = new Question(
+    q: 'Approximately one quarter of human bones are in the feet.', a: true);
+Question q1 = new Question(q: 'A slug\'s blood is green.', a: false);*/
+
+List<Question> questionBank = [
+  Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+  Question(
+      q: 'Approximately one quarter of human bones are in the feet.', a: true),
+  Question(q: 'A slug\'s blood is green.', a: false),
+];
+
+int qno = 0;
+List<Widget> scoreKeeper = [];
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questionBank[qno].quesText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                setState(() {
+                  bool corrAns = questionBank[qno].quesAns;
+                  if (corrAns == true) {
+                    Alert(
+                            context: context,
+                            title: "Correct",
+                            desc: "Congratulation")
+                        .show();
+                  } else {
+                    Alert(
+                            context: context,
+                            title: "Wrong",
+                            desc: "Try again next time")
+                        .show();
+                  }
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                  if (qno == 2)
+                    qno = 0;
+                  else
+                    qno++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                setState(() {
+                  bool corrAns = questionBank[qno].quesAns;
+                  if (corrAns == true) {
+                    Alert(
+                            context: context,
+                            title: "Correct",
+                            desc: "Congratulation")
+                        .show();
+                  } else {
+                    Alert(
+                            context: context,
+                            title: "Wrong",
+                            desc: "Try again next time")
+                        .show();
+                  }
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                  if (qno == 2)
+                    qno = 0;
+                  else
+                    qno++;
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
