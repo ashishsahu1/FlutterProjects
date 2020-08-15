e11ba34ffc5f2c950214dc6e1b25ff75f7cfa13c
ashishsahu1 <ashishsahu1999@hotmail.com>
2020-08-15 18:06:49 +0530

    startUp name Generator

---
+++ StartUpnameGen/lib/TextFieldGen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TextFieldGen {
+  TextFieldGen({String s}) {
+    TextFormField(
+      decoration: InputDecoration(labelText: 'Enter your username'),
+    );
+  }
+}
